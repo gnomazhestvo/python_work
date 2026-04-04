50bb296d74e29cd7d5cf6197591948ccb834d110
gnomazhestvo <konstantinov.lev@outlook.com>
2026-04-04 15:56:33 +0500

    11

--- Part 1/Chapter 9 - Classes/ex9.1-9.3.py
+++ Part 1/Chapter 9 - Classes/ex9.1-9.3.py
@@ -35,3 +35,86 @@ print()
 
 rest_0.describe_rest()
 rest_0.open_rest()
+
+print('\n== == == ==\n')
+
+# 9.2. Три ресторана.
+# Создайте три разных экземпляра и вызовите для каждого 
+# экземпляра метод describe_restaurant().
+print("9.2. Три ресторана.\n")
+
+class Rest2:
+    """то же самое, что и в предыдущем упражнении"""
+    def __init__(self, rest_name, cuisine_type):
+        """инициализация аргументов"""
+        self.rest_name = rest_name
+        self.cuisine_type = cuisine_type
+
+    def describe_rest(self):
+        """аргумент - селф"""
+        print(f'Ресторан - "{self.rest_name}", кухня - {self.cuisine_type}.')
+
+    def open_rest(self):
+        print(f'Ресторан "{self.rest_name}" открыт для посещения.')
+
+
+rest1 = Rest2("Naomi Campbell", "русская")
+rest2 = Rest2("Джараксус", "адская")
+rest3 = Rest2("Дрыжеглот", "болотная")
+
+rests = [rest1, rest2, rest3]
+rest_names = [rest1.rest_name, rest2.rest_name, rest3.rest_name]
+
+print(f'такс такс такс... ресторанов на выбор сегодня {len(rests)}. Это:')
+
+
+for i in range(len(rest_names)):
+    print(i+1, end='.) ')
+    rests[i].describe_rest()
+    rests[i].open_rest()
+    print()
+
+print('== == == ==\n')
+
+# 9.3. Пользователи.
+# Создайте класс User и два атрибута first_name и last_name,
+# а затем еще несколько атрибутов, которые обычно хранятся в
+# профиле пользователя. Напишите метод describe_user(), который
+# выводит сводку с информацией о пользователе. Создайте еще
+# один метод — greet_user(), позволяющий вывести персональное
+# приветствие для пользователя.
+# Создайте несколько экземпляров, представляющих разных
+# пользователей. Вызо­вите оба метода для каждого пользователя.
+print('9.3. Пользователи.\n')
+
+class User:
+    """профиль пользователя"""
+    def __init__(self, name, surname, photo='', birthday='', notification_marker='отключены'):
+        """инициализация аргументов"""
+        self.name = name
+        self.surname = surname
+        self.photo = photo
+        self.birthday = birthday
+        self.notification_marker = notification_marker
+
+    def describe_user(self):
+        print(f"Пользователь: {self.name.title()} {self.surname.title()}.")
+        if self.photo:
+            print(f"Фото: {self.photo}.")
+        if self.birthday:
+            print(f"Дата рождения: {self.birthday}.")
+        print(f"Уведомления: {self.notification_marker}")
+
+    def greet_user(self):
+            print(f"{self.name.title()} {self.surname.title()}, добро пожаловать.")
+
+user_0 = User('lev', 'volkov', 'nature', notification_marker='включены')
+user_1 = User('roman', 'smirnov', 'отсутствует', '24.09.1996', 'включены')
+user_2 = User('настя', 'шкурина', 'красоточка', '20.09.2000')
+
+users = [user_0, user_1, user_2]
+
+for user in users:
+    user.describe_user()
+    user.greet_user()
+    print()
